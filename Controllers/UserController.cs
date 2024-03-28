75668762c12399478f34e55200e5ebe93596d8a1
DIALLO <ibrahima.diallo@diotali.com>
2024-03-28 01:18:50 +0000

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace net8_webapi_jwt_token;
+    
+[ApiController]
+[Route("[/api/controller]")]
+public class UserController : Controller
+{
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
